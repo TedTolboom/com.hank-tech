43bb31567b731bd98505cfbeb38877252a1c8bf4
dT <dTNL.Homey@gmail.com>
2018-04-01 10:47:25 +0200

    Add support for HKZW-MS02

--- drivers/HKZW-MS02/device.js
+++ drivers/HKZW-MS02/device.js
@@ -4,7 +4,7 @@ const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class MotionSensor_MS02 extends ZwaveDevice {
-	onMeshInit() {
+	async onMeshInit() {
 
 		// enable debugging
 		// this.enableDebug();
@@ -14,11 +14,30 @@ class MotionSensor_MS02 extends ZwaveDevice {
 
 		// register device capabilities
 		this.registerCapability('alarm_motion', 'NOTIFICATION');
-		this.registerCapability('alarm_tamper', 'NOTIFICATION');
+		this.registerCapability('alarm_tamper', 'NOTIFICATION', {
+			report: 'NOTIFICATION_REPORT',
+			// temp hardcoding the report parser due to missing Event 9 in events.json
+			reportParser: report => {
+				if (report && report['Notification Type'] === 'Home Security' && (report.hasOwnProperty('Event (Parsed)') || report.hasOwnProperty('Event'))) {
+					if (report['Event (Parsed)'] === 'Tampering, Product covering removed' ||
+						report['Event (Parsed)'] === 'Tampering, Invalid Code' ||
+						report['Event (Parsed)'] === 'Tampering, Product Moved' ||
+						report['Event'] === 9) {
+						return true;
+					}
+					if (report['Event (Parsed)'] === 'Event inactive' && (!report.hasOwnProperty('Event Parameter') ||
+							report['Event Parameter'][0] === 3 ||
+							report['Event Parameter'][0] === 4 ||
+							report['Event Parameter'][0] === 9)) {
+						return false;
+					}
+				}
+				return null;
+			},
+		});
 		this.registerCapability('alarm_battery', 'BATTERY');
 		this.registerCapability('measure_battery', 'BATTERY');
 	}
-
 }
 
 module.exports = MotionSensor_MS02;
