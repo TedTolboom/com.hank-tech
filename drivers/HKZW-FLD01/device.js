ad3308cee81e76598417af87aa1675b1c5c1bcf8
dT <dTNL.Homey@gmail.com>
2018-04-01 10:47:41 +0200

    Add support for HKZW-FLD01

--- drivers/HKZW-FLD01/device.js
+++ drivers/HKZW-FLD01/device.js
@@ -13,7 +13,27 @@ class FloodSensor_FLD01 extends ZwaveDevice {
 		// register device capabilities
 		this.registerCapability('alarm_water', 'NOTIFICATION');
 		this.registerCapability('alarm_heat', 'NOTIFICATION');
-		this.registerCapability('alarm_tamper', 'NOTIFICATION');
+		this.registerCapability('alarm_tamper', 'NOTIFICATION', {
+			report: 'NOTIFICATION_REPORT',
+			// temp hardcoding the report parser due to missing Event 9 in events.json
+			reportParser: report => {
+				if (report && report['Notification Type'] === 'Home Security' && (report.hasOwnProperty('Event (Parsed)') || report.hasOwnProperty('Event'))) {
+					if (report['Event (Parsed)'] === 'Tampering, Product covering removed' ||
+						report['Event (Parsed)'] === 'Tampering, Invalid Code' ||
+						report['Event (Parsed)'] === 'Tampering, Product Moved' ||
+						report['Event'] === 9) {
+						return true;
+					}
+					if (report['Event (Parsed)'] === 'Event inactive' && (!report.hasOwnProperty('Event Parameter') ||
+							report['Event Parameter'][0] === 3 ||
+							report['Event Parameter'][0] === 4 ||
+							report['Event Parameter'][0] === 9)) {
+						return false;
+					}
+				}
+				return null;
+			},
+		});
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
 		this.registerCapability('alarm_battery', 'BATTERY');
 		this.registerCapability('measure_battery', 'BATTERY');
