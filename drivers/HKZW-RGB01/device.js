e13ff95ffae08af187b182646c0052a86579337f
dT <dTNL.Homey@gmail.com>
2018-04-01 12:04:37 +0200

    Manual merge Caseda/RGB01

---
+++ drivers/HKZW-RGB01/device.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Homey = require('homey');
+const ZwaveXYLightDevice = require('homey-meshdriver').ZwaveXYLightDevice;
+
+class ColorBulb_RGB01 extends ZwaveXYLightDevice {
+	async onMeshInit() {
+		await super.onMeshInit();
+
+		process.on('unhandledRejection', error => {
+			this.error('unhandledRejection', error);
+		});
+
+		// enable debugging
+		this.enableDebug();
+
+		// print the node's info to the console
+		// this.printNode();
+
+		// Add the BASIC report listener for onoff and dim capabilities as the device reports in BASIC
+		this.registerReportListener('BASIC', 'BASIC_REPORT', report => {
+			if (report.hasOwnProperty('Value')) {
+				this.setCapabilityValue('onoff', report.Value > 0);
+				this.setCapabilityValue('dim', report.Value / 99);
+			}
+		});
+	}
+}
+
+module.exports = ColorBulb_RGB01;
