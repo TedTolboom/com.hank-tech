9e3ad57a9d630cee6970daca9fcfa6849935083a
dT <dTNL.Homey@gmail.com>
2018-11-14 22:20:36 +0100

    Add start-level and stop-level dim cards to RGB-bulb

--- drivers/HKZW-RGB01/device.js
+++ drivers/HKZW-RGB01/device.js
@@ -1,10 +1,24 @@
 'use strict';
 
-const ZwaveXYLightDevice = require('homey-meshdriver').ZwaveXYLightDevice;
+const ZwaveLightDevice = require('homey-meshdriver').ZwaveLightDevice;
 
-class ColorBulb_RGB01 extends ZwaveXYLightDevice {
+class ColorBulb_RGB01 extends ZwaveLightDevice {
 	async onMeshInit() {
 		await super.onMeshInit();
+
+		// enable debugging
+		this.enableDebug();
+
+		// print the node's info to the console
+		this.printNode();
+
+		// register a report listener
+		this.registerReportListener('BASIC', 'BASIC_REPORT', (rawReport, parsedReport) => {
+			if (rawReport && rawReport.hasOwnProperty('Value')) {
+				if (this.hasCapability('onoff')) this.setCapabilityValue('onoff', rawReport.Value > 0);
+				if (this.hasCapability('dim')) this.setCapabilityValue('dim', rawReport.Value > 99 ? 1 : rawReport.Value / 99);
+			}
+		});
 	}
 }
 
